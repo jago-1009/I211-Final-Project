e4e1977083362aff39793df17319f4819807d955
nateosborne <natedresdeno@gmail.com>
2023-11-15 02:24:29 -0500

    views
    views

--- views/index_view.class.php
+++ views/index_view.class.php
@@ -8,3 +8,26 @@
  */
 
 
+class View {
+static public function displayheader($page_title) {
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <title><?php echo $page_title ?></title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
+<?php
+}
+
+}
+
+?>
---
+++ views/photo/photo_index.class.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Author: Nate Osborne
+ * Date: 11/15/2023
+ * File Name: photo_index.class.php
+ * Description:
+ */
+
+class PhotoView extends View {
+    public function display($photos){
+        parent::displayheader("Photo Library");
+        ?>
+
+        <div class="header">Photographs</div>
+
+        <div class="container">
+            <?php
+            if($photos === 0){
+                echo "No photo was found.";
+            } else {
+                foreach ($photos as $photo){
+                    $id = $photo->getPhotoId();
+                    $size = $photo->getSize();
+                    $camera = $photo->getCamera();
+                    $decription = $photo->getDescription();
+                }
+            }
+            ?>
+        </div>
+<?php
+        parent::displayfooter();
+    }
+}
+?>
