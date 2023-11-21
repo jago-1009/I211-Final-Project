b49d51ce6878f131f23a75da71a307424c74b413
jago-1009 <garwoodj22@gmail.com>
2023-11-21 10:41:59 -0500

    Update config.php

--- application/config.php
+++ application/config.php
@@ -4,4 +4,21 @@
  * Date: 11/7/2023
  * File: config.php
  * Description:
- */ 
+ */
+//error reporting for reporting all errors
+error_reporting(E_ALL);
+
+//local time zone
+date_default_timezone_set("America/New_York");
+
+//Base URL
+define("BASE_URL", "http://localhost/i211/I211-Final-Project");
+
+
+
+
+///*************************************************************************************
+// *                       settings for Photos                                        *
+// ************************************************************************************/
+//
+//define("MOVIE_IMG", "www/img/");
