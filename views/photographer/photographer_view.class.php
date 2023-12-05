1a7b5c212ecc96a67695b94130a65ac2e2e2c62e
nateosborne <natedresdeno@gmail.com>
2023-12-05 11:03:46 -0500

    photographer link
    added link to photographers

--- views/photographer/photographer_view.class.php
+++ views/photographer/photographer_view.class.php
@@ -28,7 +28,7 @@ class PhotographerView extends View {
                     $email = $photographers->getEmail();
 
                     echo "<div class='photographer-list'>
-                        <div class='photographer-list-details'><h1>$firstname $lastname</h1><h4>$birthdate</h4><h4>$email</h4><p>ID: $photographerID</p></div>
+                        <div class='photographer-list-details'><a href='", BASE_URL, "/photographer/detail/$photographerID'><h1>$firstname $lastname</h1></a><h4>$birthdate</h4><h4>$email</h4><p>ID: $photographerID</p></div>
                     </div>";
                 }
             }
